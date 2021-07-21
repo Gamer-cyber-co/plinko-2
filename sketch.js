6089b471e6b1f50e71aa0152395194c30be8f414
Gamer-cyber-co <83217973+Gamer-cyber-co@users.noreply.github.com>
2021-07-21 16:23:35 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -86,7 +86,33 @@ function draw() {
    }
    if (ball!=null) {
    ball.display();
-}}
+}
+if(ball!=null)
+ {
+ ball.display();
+
+ if (ball.body.position.y>760)
+ {
+ if (ball.body.position.x < 300)
+ {
+ score=score+500;
+ ball=null;
+ }
+ else if (ball.body.position.x < 600 && ball.body.position.x > 301 )
+ {
+ score = score + 100;
+ ball=null;
+
+ }
+ else if (ball.body.position.x < 900 && ball.body.position.x > 601 )
+ {
+ score = score + 200;
+ ball=null;
+ }
+
+ }
+}
+}
 
 function mousePressed()
 {
